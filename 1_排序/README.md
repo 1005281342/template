1fa6be2e5ccce5020550ebdbd4e8d5e9e841a0f8
1005281342@qq.com <1005281342@qq.com>
2020-06-17 00:16:49 +0800

    添加基于双指针实现的快速排序模版

---
+++ 1_排序/README.md
@@ -0,0 +1,5 @@
+---
+title: 排序
+---
+
+[前往LeetCode进行验证](https://leetcode-cn.com/problems/sort-an-array/)
--- 1_排序/快速排序/python3/main.py
+++ 1_排序/快速排序/python3/main.py
@@ -1,6 +1,7 @@
 from random import randint
 
 
+# 借助额外数组，容易理解
 def quick_sort_with_help(nums: list) -> list:
     if len(nums) <= 1:
         return nums
@@ -26,7 +27,8 @@ def quick_sort_with_help(nums: list) -> list:
     return low
 
 
-def quick_sort(nums: list, start: int, end: int):
+# 参考《数据结构与算法基于Python实现》
+def quick_sort_p(nums: list, start: int, end: int):
     if start >= end:
         return
 
@@ -46,8 +48,34 @@ def quick_sort(nums: list, start: int, end: int):
             left += 1
             right -= 1
     nums[left], nums[end] = nums[end], nums[left]
-    quick_sort(nums, start, left - 1)
-    quick_sort(nums, left + 1, end)
+    quick_sort_p(nums, start, left - 1)
+    quick_sort_p(nums, left + 1, end)
+
+
+# from acWing by yxc(c++ => python3)
+def quick_sort(nums: list, start: int, end: int):
+    if start >= end:
+        return
+
+    # 双指针
+    left, right = start - 1, end + 1
+
+    t = nums[left + ((right - left) >> 1)]
+
+    while left < right:
+        left += 1
+        while nums[left] < t:
+            left += 1
+
+        right -= 1
+        while nums[right] > t:
+            right -= 1
+
+        if left < right:
+            nums[left], nums[right] = nums[right], nums[left]
+
+    quick_sort(nums, start, right)
+    quick_sort(nums, right + 1, end)
 
 
 if __name__ == '__main__':
@@ -56,5 +84,10 @@ if __name__ == '__main__':
     print(quick_sort_with_help(case))
 
     print(case)
-    quick_sort(case, 0, len(case)-1)
+    quick_sort_p(case, 0, len(case) - 1)
+    print(case)
+
+    case = [2, 4, 2, 3, 10, 99, 4, 6, 2, 1, 5, 77, 2]
+    print(case)
+    quick_sort(case, 0, len(case) - 1)
     print(case)
